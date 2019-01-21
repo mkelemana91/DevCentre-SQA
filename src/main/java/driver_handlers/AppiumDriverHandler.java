a46ceb0a6a33278e5702b38ba3dae7bceb6c780c
mkelemana91 <42268009+mkelemana91@users.noreply.github.com>
2019-01-21 11:29:45 +0200

    Mobile automation - first commit

--- src/main/java/driver_handlers/AppiumDriverHandler.java
+++ src/main/java/driver_handlers/AppiumDriverHandler.java
@@ -3,7 +3,10 @@ package driver_handlers;
 import io.appium.java_client.AppiumDriver;
 import io.appium.java_client.MobileElement;
 import io.appium.java_client.android.AndroidDriver;
+import io.appium.java_client.remote.MobileBrowserType;
 import io.appium.java_client.remote.MobileCapabilityType;
+import io.appium.java_client.remote.MobilePlatform;
+import io.appium.java_client.service.local.AppiumDriverLocalService;
 import org.openqa.selenium.remote.DesiredCapabilities;
 
 import java.io.FileInputStream;
@@ -12,47 +15,56 @@ import java.io.IOException;
 import java.net.MalformedURLException;
 import java.net.URL;
 import java.util.Properties;
+import java.util.concurrent.TimeUnit;
 
 public class AppiumDriverHandler {
 
-    private static Properties mobileProp;
-    private static AppiumDriver<MobileElement> mobileDriver;
+    public static Properties mobileProp = null;
+    public static AppiumDriver<MobileElement> mobileDriver = null;
 
 
 
     public AppiumDriverHandler(){
 
-        mobileProp = new Properties();
 
 
-        try {
-            FileInputStream propFile = new FileInputStream(System.getProperty("user.dir")+
-                    "/src/main/resources/mobileconfig.properties");
-            mobileProp.load(propFile);
-        } catch (FileNotFoundException e) {
-            e.printStackTrace();
-        } catch (IOException e) {
-            e.printStackTrace();
+        if(mobileProp == null) {
+            mobileProp = new Properties();
+            try {
+                FileInputStream propFile = new FileInputStream(System.getProperty("user.dir") +
+                        "/src/main/resources/mobileconfig.properties");
+                mobileProp.load(propFile);
+            } catch (FileNotFoundException e) {
+                e.printStackTrace();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+
+        if(mobileDriver == null) {
+            //Settings for our mobile device
+            DesiredCapabilities capabilities = new DesiredCapabilities();
+
+            capabilities.setCapability(MobileCapabilityType.UDID, mobileProp.getProperty("udid"));
+            capabilities.setCapability(MobileCapabilityType.DEVICE_NAME, mobileProp.getProperty("deviceName"));
+            capabilities.setCapability(MobileCapabilityType.PLATFORM_NAME, mobileProp.getProperty("platformName"));
+            capabilities.setCapability(MobileCapabilityType.PLATFORM_VERSION, mobileProp.getProperty("platformVersion"));
+            //capabilities.setCapability(MobileCapabilityType.APP,System.getProperty("user.dir")+"/apk/apkName.apk");
+            capabilities.setCapability(MobileCapabilityType.BROWSER_NAME, mobileProp.getProperty("browser"));
+
+            try {
+                //service.start();
+                mobileDriver = new AndroidDriver<MobileElement>(new URL("http://0.0.0.0:4723/wd/hub"), capabilities);
+
+
+            } catch (MalformedURLException e) {
+                e.printStackTrace();
+            }
+        }
         }
     }
     public void launchApp(){
-        DesiredCapabilities capabilities = new DesiredCapabilities();
-
-        //capabilities.setCapability(MobileCapabilityType.UDID,mobileProp.getProperty("udid"));
-        capabilities.setCapability(MobileCapabilityType.DEVICE_NAME,mobileProp.getProperty("deviceName"));
-        capabilities.setCapability(MobileCapabilityType.PLATFORM_NAME,mobileProp.getProperty("platformName"));
-        capabilities.setCapability(MobileCapabilityType.PLATFORM_VERSION,mobileProp.getProperty("platformVersion"));
-        capabilities.setCapability(MobileCapabilityType.APP,System.getProperty("user.dir")+"/apk/apkName.apk");
-
-        //mobileDriver = new AndroidDriver<MobileElement>(capabilities);
-        try {
-
-            mobileDriver = new AndroidDriver<MobileElement>(new URL("http://0.0.0.0:4723/wd/hub"), capabilities);
-
-
-        } catch (MalformedURLException e) {
-            e.printStackTrace();
-        }
+        mobileDriver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
+        mobileDriver.get("https://eohmc-omwealth-mobile.azurewebsites.net");
     }
 
     public void closeApp(){
--- src/main/java/driver_handlers/WebDriverHandler.java
+++ src/main/java/driver_handlers/WebDriverHandler.java
@@ -3,6 +3,7 @@ package driver_handlers;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.support.ui.WebDriverWait;
 
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
@@ -12,42 +13,52 @@ import java.util.concurrent.TimeUnit;
 
 public class WebDriverHandler {
 
-    public static WebDriver driver;
-    public static Properties prop;
+    public static WebDriver driver = null;
+    public static Properties prop = null;
+    public static WebDriverWait wait;
 
     public WebDriverHandler() {
+        if (prop == null) {
+            prop = new Properties();
 
-        prop = new Properties();
+            try {
+                FileInputStream propFile = new FileInputStream(System.getProperty("user.dir") +
+                        "/src/main/resources/webconfig.properties");
+                prop.load(propFile);
+            } catch (FileNotFoundException e) {
+                e.printStackTrace();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
 
-        try {
-            FileInputStream propFile = new FileInputStream(System.getProperty("user.dir")+
-                    "/src/main/resources/webconfig.properties");
-            prop.load(propFile);
-        } catch (FileNotFoundException e) {
-            e.printStackTrace();
-        } catch (IOException e) {
-            e.printStackTrace();
+            String browserName = prop.getProperty("browser");
+
+            if (driver == null) {
+                if ("chrome".equalsIgnoreCase(browserName)) {
+                    driver = new ChromeDriver();
+
+                } else if ("firefox".equalsIgnoreCase(browserName)) {
+                    driver = new FirefoxDriver();
+
+                } else {
+                    System.out.print("unable to start " + browserName + " browser");
+                }
+            }
         }
     }
 
-    public void startBrowser(){
+    public void startBrowser() {
 
-        String browserName = prop.getProperty("browser");
-
-        if("chrome".equalsIgnoreCase(browserName))
-            driver = new ChromeDriver();
-        else if("firefox".equalsIgnoreCase(browserName))
-            driver = new FirefoxDriver();
-        else
-            System.out.print("unable to start " + browserName + " browser");
 
         driver.manage().window().maximize();
         driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
+        //driver.manage().timeouts().pageLoadTimeout(30, TimeUnit.SECONDS);
+        wait = new WebDriverWait(driver, 5);
         driver.get(prop.getProperty("url"));
 
     }
 
-    public void closeBrowser(){
+    public void closeBrowser() {
 
         driver.manage().deleteAllCookies();
         //driver.close();
